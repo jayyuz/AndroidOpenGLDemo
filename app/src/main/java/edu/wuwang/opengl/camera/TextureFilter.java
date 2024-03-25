72dcb05b4662d2a461f0715d42cf15d4b245b4c1
jaesonzhang <jaesonzhang@tencent.com>
2024-03-25 21:35:00 +0800

    格式化代码等等

--- app/src/main/java/edu/wuwang/opengl/camera/TextureFilter.java
+++ app/src/main/java/edu/wuwang/opengl/camera/TextureFilter.java
@@ -1,7 +1,7 @@
 /*
  *
  * TrackFilter.java
- * 
+ *
  * Created by Wuwang on 2016/12/21
  * Copyright © 2016年 深圳哎吖科技. All rights reserved.
  */
@@ -25,29 +25,29 @@ import edu.wuwang.opengl.utils.EasyGlUtils;
 public class TextureFilter extends AFilter {
 
     private CameraFilter mFilter;
-    private int width=0;
-    private int height=0;
+    private int width = 0;
+    private int height = 0;
 
     private int[] fFrame = new int[1];
     private int[] fTexture = new int[1];
-    private int[] mCameraTexture=new int[1];
+    private int[] mCameraTexture = new int[1];
 
     private SurfaceTexture mSurfaceTexture;
-    private float[] mCoordOM=new float[16];
+    private float[] mCoordOM = new float[16];
 
     //获取Track数据
     private ByteBuffer tBuffer;
 
     public TextureFilter(Resources mRes) {
         super(mRes);
-        mFilter=new CameraFilter(mRes);
+        mFilter = new CameraFilter(mRes);
     }
 
-    public void setCoordMatrix(float[] matrix){
+    public void setCoordMatrix(float[] matrix) {
         mFilter.setCoordMatrix(matrix);
     }
 
-    public SurfaceTexture getTexture(){
+    public SurfaceTexture getTexture() {
         return mSurfaceTexture;
     }
 
@@ -73,23 +73,23 @@ public class TextureFilter extends AFilter {
 
     @Override
     public void draw() {
-        boolean a=GLES20.glIsEnabled(GLES20.GL_DEPTH_TEST);
-        if(a){
+        boolean a = GLES20.glIsEnabled(GLES20.GL_DEPTH_TEST);
+        if (a) {
             GLES20.glDisable(GLES20.GL_DEPTH_TEST);
         }
-        if(mSurfaceTexture!=null){
+        if (mSurfaceTexture != null) {
             mSurfaceTexture.updateTexImage();
             mSurfaceTexture.getTransformMatrix(mCoordOM);
             mFilter.setCoordMatrix(mCoordOM);
         }
-        EasyGlUtils.bindFrameTexture(fFrame[0],fTexture[0]);
-        GLES20.glViewport(0,0,width,height);
+        EasyGlUtils.bindFrameTexture(fFrame[0], fTexture[0]);
+        GLES20.glViewport(0, 0, width, height);
         mFilter.setTextureId(mCameraTexture[0]);
         mFilter.draw();
-        Log.e("wuwang","textureFilter draw");
+        Log.e("wuwang", "textureFilter draw");
         EasyGlUtils.unBindFrameBuffer();
 
-        if(a){
+        if (a) {
             GLES20.glEnable(GLES20.GL_DEPTH_TEST);
         }
     }
@@ -98,29 +98,29 @@ public class TextureFilter extends AFilter {
     protected void onCreate() {
         mFilter.create();
         createOesTexture();
-        mSurfaceTexture=new SurfaceTexture(mCameraTexture[0]);
+        mSurfaceTexture = new SurfaceTexture(mCameraTexture[0]);
     }
 
     @Override
     protected void onSizeChanged(int width, int height) {
         mFilter.setSize(width, height);
-        if(this.width!=width||this.height!=height){
-            this.width=width;
-            this.height=height;
+        if (this.width != width || this.height != height) {
+            this.width = width;
+            this.height = height;
             //创建FrameBuffer和Texture
             deleteFrameBuffer();
             GLES20.glGenFramebuffers(1, fFrame, 0);
-            EasyGlUtils.genTexturesWithParameter(1, fTexture,0,GLES20.GL_RGBA,width,height);
+            EasyGlUtils.genTexturesWithParameter(1, fTexture, 0, GLES20.GL_RGBA, width, height);
         }
     }
 
     private void deleteFrameBuffer() {
         GLES20.glDeleteFramebuffers(1, fFrame, 0);
-        GLES20.glDeleteTextures(1, fTexture,0);
+        GLES20.glDeleteTextures(1, fTexture, 0);
     }
 
-    private void createOesTexture(){
-        GLES20.glGenTextures(1,mCameraTexture,0);
+    private void createOesTexture() {
+        GLES20.glGenTextures(1, mCameraTexture, 0);
     }
 
 }
